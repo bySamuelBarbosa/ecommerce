a648775e0a25074e1e02c5974ba2412739ab87a9
SamuelBarbosa <samuelbarbosa9090@gmail.com>
2024-07-15 21:03:29 -0300

    Feat: Creating Category and Brand resources

--- public/index.php
+++ public/index.php
@@ -4,8 +4,6 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-date_default_timezone_set('America/Sao_Paulo');
-
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
