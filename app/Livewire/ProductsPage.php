0ed9c7ee33602cb76776e5da9d39bfc0d66de863
SamuelBarbosa <samuelbarbosa9090@gmail.com>
2024-08-26 19:49:44 -0300

    Feat: Dynamic filters on products page

--- app/Livewire/ProductsPage.php
+++ app/Livewire/ProductsPage.php
@@ -7,6 +7,7 @@ use App\Models\Category;
 use App\Models\Product;
 use Livewire\Component;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 
 #[Title('Products - DCodeMania')]
@@ -14,11 +15,57 @@ class ProductsPage extends Component
 {
     use WithPagination;
 
+    #[Url]
+    public $selected_categories = [];
+
+    #[Url]
+    public $selected_brands = [];
+
+    #[Url]
+    public $featured;
+
+    #[Url]
+    public $on_sale;
+
+    #[Url]
+    public $price_range = 9999;
+
+    public $maxPrice;
+
     public function render()
     {
+        $getMaxPrice = Product::query()
+            ->select('price')
+            ->orderByDesc('price')
+            ->first()
+            ->price;
+
+        $this->maxPrice = $getMaxPrice + 1000;
+
         $productQuery = Product::query()->where('is_active', 1);
+
+        if(!empty($this->selected_categories)){
+            $productQuery->whereIn('category_id', $this->selected_categories);
+        }
+
+        if(!empty($this->selected_brands)){
+            $productQuery->whereIn('brand_id', $this->selected_brands);
+        }
+
+        if($this->featured){
+            $productQuery->where('is_featured', 1);
+        }
+
+        if($this->on_sale){
+            $productQuery->where('on_sale', 1);
+        }
+
+        if($this->price_range){
+            $productQuery->whereBetween('price', [0, $this->price_range]);
+        }
+
         return view('livewire.products-page', [
-            'products' => $productQuery->paginate(1),
+            'products' => $productQuery->paginate(9),
             'brands' => Brand::where('is_active', 1)->get(['id', 'name', 'slug']),
             'categories' => Category::where('is_active', 1)->get(['id', 'name', 'slug']),
         ]);
